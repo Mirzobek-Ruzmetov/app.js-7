c0bc4e5257acee1dec905c2e03c5f3eb70b8fe84
Mirzobek Ruzmetov <ruzmetovmirzobek10@gmail.com>
2026-03-02 22:08:46 +0500

    app.js

--- app.js
+++ app.js
@@ -16,24 +16,3 @@ function calculate(a, b) {
     divide: divide
   };
 }
-
-let result = calculate(12, 4);
-
-console.log("Yig'indi:", result.sum);
-console.log("Ayirma:", result.minus);
-console.log("Ko'paytma:", result.multiply);
-console.log("Bo'linma:", result.divide);
-
-let numbers = [5, 10, 15, 20];
-
-numbers.forEach(function(num) {
-  console.log("Son:", num);
-});
-
-if (age >= 18) {
-  console.log("Siz katta yoshdasiz");
-} else {
-  console.log("Siz hali voyaga yetmagansiz");
-}
-
-console.log("Dastur ishladi");
